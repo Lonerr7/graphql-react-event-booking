091bfa77740c2ab00c73bd24c068ac81add7731c
Lonerr7 <krabik2342@gmail.com>
2023-06-04 11:09:45 +0300

    4. Connected server to DB. Added logic to retrieve data from db and to create a new event and store it in db

--- app.js
+++ app.js
@@ -2,12 +2,17 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import { graphqlHTTP } from 'express-graphql';
 import { buildSchema } from 'graphql';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import { Event } from './models/eventModel.js';
 
 const app = express();
 
 app.use(bodyParser.json());
 
 const events = [];
+
+dotenv.config({ path: './config.env' });
 
 app.use(
   '/graphql',
@@ -42,27 +47,46 @@ app.use(
         }
     `),
     rootValue: {
-      events: () => {
-        return events;
+      events: async () => {
+        return await Event.find();
       },
-      createEvent: (args) => {
+      createEvent: async (args) => {
         const { title, description, price, date } = args.eventInput;
 
-        const newEvent = {
-          _id: Math.random().toString(),
+        const newEvent = await Event.create({
           title,
           description,
           price: +price,
-          date: date,
-        };
-
-        events.push(newEvent);
+          date: new Date(date),
+        });
 
         return newEvent;
+
+        // return newEvent
+        //   .save()
+        //   .then((result) => {
+        //     console.log(result);
+        //     return { ...result._doc };
+        //   })
+        //   .catch((err) => {
+        //     console.log(err);
+        //     throw err;
+        //   });
       },
     }, // all resolver functions (they need to match our schema endpoints by name)
     graphiql: true,
   })
 );
 
-app.listen(4000);
+const connectionLink = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@graphqlcluster.burlevo.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`;
+
+mongoose
+  .connect(connectionLink)
+  .then(() => {
+    // Starting a server only when we successfully connected to mongoDB
+    console.log(`successfully connected to MONGODB`);
+    app.listen(4000);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
---
+++ models/eventModel.js
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose';
+
+const EventSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  date: {
+    type: Date,
+    required: true,
+  },
+});
+
+export const Event = mongoose.model('Event', EventSchema);
